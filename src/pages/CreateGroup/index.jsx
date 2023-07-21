35ce948773a2f2010e817581d71f20c3d89dd340
geniee1220 <geniee1220@gmail.com>
2023-07-21 21:50:58 +0900

    Feat : react-hook-form 적용

--- src/pages/CreateGroup/index.jsx
+++ src/pages/CreateGroup/index.jsx
@@ -1,10 +1,31 @@
 import { useRecoilState } from 'recoil';
 import { groupNameState } from '../../state/groupState';
 
+import { useForm } from 'react-hook-form';
+
 import CenteredOverlayForm from '../../components/CenteredOverlayForm';
+import { useEffect } from 'react';
 
 function CreateGroup() {
   const [groupName, setGroupName] = useRecoilState(groupNameState);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  // 디버깅용 코드
+  useEffect(() => {
+    console.log(groupName);
+  }, [groupName, setGroupName]);
+
+  const onSubmit = (data) => {
+    setGroupName(data.groupName);
+  };
 
   return (
     <div className="min-h-screen relative bg-blue-100">
@@ -17,8 +38,8 @@ function CreateGroup() {
               alt="Cover"
             />
           </figure>
-          <div className="card-body">
-            <form className="w-full max-w-lg flex flex-col m-auto">
+          <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+            <div className="w-full max-w-lg flex flex-col mt-[200px]">
               <p className="w-full text-4xl font-medium text-center leading-snug">
                 Splity 시작하기
               </p>
@@ -27,15 +48,24 @@ function CreateGroup() {
                 placeholder="더치 페이 그룹의 이름을 입력해주세요"
                 className="input input-bordered w-full mt-10"
                 spellCheck="false"
-                required
+                autoComplete="off"
+                {...register('groupName', {
+                  required: '그룹 이름은 빈 칸으로 비워둘 수 없습니다',
+                })}
               />
-            </form>
-            <div className="card-actions justify-end">
+
+              {errors.groupName && (
+                <p className="text-red-500 text-sm mt-2">
+                  {errors.groupName.message}
+                </p>
+              )}
+            </div>
+            <div className="card-actions justify-end mt-auto">
               <button className="btn btn-primary px-4 min-w-[100px]">
                 저장
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
