b856848add2274295ae9b8a4f2d604d0680d7a55
geniee1220 <geniee1220@gmail.com>
2023-07-23 21:38:49 +0900

    Test : 에러 메세지 체크 함수 추가 및 빈 값 저장 테스트 시나리오 추가

--- src/pages/CreateGroup/CreateGroup.spec.jsx
+++ src/pages/CreateGroup/CreateGroup.spec.jsx
@@ -14,11 +14,10 @@ const renderComponent = () => {
     '더치 페이 그룹의 이름을 입력해주세요'
   );
   const saveButton = screen.getByText('저장');
-  const errorMessage = screen.queryByText(
-    '그룹 이름은 빈 칸으로 비워둘 수 없습니다'
-  );
+  const checkErrorMessage = () =>
+    screen.queryByText('그룹 이름은 빈 칸으로 비워둘 수 없습니다');
 
-  return { input, saveButton, errorMessage };
+  return { input, saveButton, checkErrorMessage };
 };
 
 describe('그룹 생성 페이지', () => {
@@ -29,13 +28,25 @@ describe('그룹 생성 페이지', () => {
     expect(saveButton).not.toBeNull();
   });
 
+  test('그룹 이름을 입력하지 않은 상태에서 저장 버튼 클릭 시, 저장 실패', async () => {
+    const { saveButton, checkErrorMessage } = renderComponent();
+
+    userEvent.click(saveButton);
+
+    await waitFor(() => {
+      const errorMessage = checkErrorMessage();
+      expect(errorMessage).toBeInTheDocument();
+    });
+  });
+
   test('그룹 이름을 입력한 이후에 저장 버튼 클릭 시, 저장 성공', async () => {
-    const { input, saveButton, errorMessage } = renderComponent();
+    const { input, saveButton, checkErrorMessage } = renderComponent();
 
     await userEvent.type(input, '그룹 이름');
     await userEvent.click(saveButton);
 
     await waitFor(() => {
+      const errorMessage = checkErrorMessage();
       expect(errorMessage).toBeNull();
     });
   });
