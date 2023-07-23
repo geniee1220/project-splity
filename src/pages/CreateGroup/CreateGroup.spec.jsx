8bc916f40fbab0498a7a0b2460f053d6da7d11a5
geniee1220 <geniee1220@gmail.com>
2023-07-23 21:12:49 +0900

    Test : 그룹 생성 테스트 오류 검사 후 수정

--- src/pages/CreateGroup/CreateGroup.spec.jsx
+++ src/pages/CreateGroup/CreateGroup.spec.jsx
@@ -1,7 +1,7 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import CreateGroup from '.';
-import userEvent from '@testing-library/user-event';
 import { RecoilRoot } from 'recoil';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CreateGroup from '.';
 
 const renderComponent = () => {
   render(
@@ -9,6 +9,7 @@ const renderComponent = () => {
       <CreateGroup />
     </RecoilRoot>
   );
+
   const input = screen.getByPlaceholderText(
     '더치 페이 그룹의 이름을 입력해주세요'
   );
@@ -23,6 +24,7 @@ const renderComponent = () => {
 describe('그룹 생성 페이지', () => {
   test('그룹 입력 컴포넌트 렌더링 확인', () => {
     const { input, saveButton } = renderComponent();
+
     expect(input).not.toBeNull();
     expect(saveButton).not.toBeNull();
   });
@@ -30,8 +32,8 @@ describe('그룹 생성 페이지', () => {
   test('그룹 이름을 입력한 이후에 저장 버튼 클릭 시, 저장 성공', async () => {
     const { input, saveButton, errorMessage } = renderComponent();
 
-    userEvent.type(input, '그룹 이름');
-    userEvent.click(saveButton);
+    await userEvent.type(input, '그룹 이름');
+    await userEvent.click(saveButton);
 
     await waitFor(() => {
       expect(errorMessage).toBeNull();
