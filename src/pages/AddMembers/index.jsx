a0fa135180038144ea3690cb64357cabc2b0278d
geniee1220 <geniee1220@gmail.com>
2023-07-23 23:10:33 +0900

    Feat : 그룹 멤버 추가 페이지

--- src/pages/AddMembers/index.jsx
+++ src/pages/AddMembers/index.jsx
@@ -1,7 +1,160 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { groupMembersState } from '../../state/groupState';
+
+import { useForm } from 'react-hook-form';
+
+import { addMember, deleteMember } from '../../utils/member';
+
+import CenteredOverlayForm from '../../components/CenteredOverlayForm';
 
 function AddMembers() {
-  return <div>AddMembers Component</div>;
+  const [groupMembers, setGroupMembers] = useRecoilState(groupMembersState);
+
+  const [members, setMembers] = useState([]);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    setError,
+    setFocus,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+  });
+
+  const handleFormKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
+  };
+
+  // 태그 추가
+  const handleKeyUp = (event) => {
+    const inputTag = watch('memberName');
+    const newMembers = addMember(
+      members,
+      inputTag,
+      event,
+      setValue,
+      setFocus,
+      setError
+    );
+    setMembers(newMembers);
+  };
+
+  // 태그 삭제 버튼 클릭 시 태그 삭제
+  const handleTagDelete = (member) => {
+    setMembers(deleteMember(members, member));
+  };
+
+  const onSubmit = async (data) => {
+    if (members.length === 0) {
+      setError('memberName', {
+        type: 'vaildation',
+        message: '멤버 이름은 빈 칸으로 비워둘 수 없습니다',
+      });
+      return;
+    }
+
+    setGroupMembers(members);
+  };
+
+  useEffect(() => {
+    const subscription = watch((value, { name, type }) =>
+      console.log(value, name, type)
+    );
+    return () => subscription.unsubscribe();
+  }, [watch]);
+
+  return (
+    <div className="min-h-screen relative bg-blue-100">
+      <CenteredOverlayForm>
+        <figure className="max-w-screen-sm">
+          <img
+            src="https://images.unsplash.com/photo-1646343961743-ef39d2b02c21?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGlsbHVzdHJhdGlvbiUyMHllbGxvd3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            alt="Cover"
+          />
+        </figure>
+
+        {/* 폼 */}
+        <form
+          className="card-body"
+          onSubmit={handleSubmit(onSubmit)}
+          onKeyDown={handleFormKeyDown}
+        >
+          <div className="w-full max-w-lg flex flex-col mt-[200px]">
+            <p className="w-full text-4xl font-medium text-center leading-snug">
+              Splity 시작하기
+            </p>
+
+            {/* 멤버 입력 */}
+            <div className="mt-10">
+              <p className="text-sm text-gray-600">
+                멤버 목록 {members.length}
+              </p>
+
+              {members.map((member, index) => (
+                <div
+                  key={index}
+                  className="inline-flex items-center mt-2 bg-blue-500 bg-opacity-10 mr-2 px-2 rounded"
+                >
+                  <span className="text-sm font-medium text-gray-700 mr-1">
+                    {member}
+                  </span>
+                  <button
+                    className="btn btn-ghost btn-sm pl-0 pr-0"
+                    onClick={() => handleTagDelete(member)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="icon icon-tabler icon-tabler-x"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="#ff0000"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" />
+                      <line x1="18" y1="6" x2="6" y2="18" />
+                      <line x1="6" y1="6" x2="18" y2="18" />
+                    </svg>
+                  </button>
+                </div>
+              ))}
+            </div>
+
+            <input
+              type="text"
+              data-testid="input-member-names"
+              placeholder="더치 페이 그룹 멤버의 이름을 입력해주세요"
+              className="input input-bordered w-full mt-4"
+              spellCheck="false"
+              autoComplete="off"
+              onKeyUp={handleKeyUp}
+              {...register('memberName')}
+            />
+
+            {errors.memberName && (
+              <p className="text-red-500 text-sm mt-2">
+                {errors.memberName.message}
+              </p>
+            )}
+          </div>
+          <div className="card-actions justify-end mt-auto">
+            <button className="btn btn-primary px-4 min-w-[100px] mt-4">
+              저장
+            </button>
+          </div>
+        </form>
+      </CenteredOverlayForm>
+    </div>
+  );
 }
 
 export default AddMembers;
